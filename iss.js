8b983010eaf0cce056c7112b184c2ae1cce2c9e7
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-12-09 13:03:19 -0500

    Implemented fetchISSFlyOverTimes

--- iss.js
+++ iss.js
@@ -40,5 +40,25 @@ const fetchCoordsByIP = (ip, callback) => {
   });
 };
 
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
+module.exports = { fetchISSFlyOverTimes };
 module.exports = { fetchMyIP };
 module.exports = { fetchCoordsByIP };
