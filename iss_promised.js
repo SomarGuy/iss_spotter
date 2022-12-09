243a800a876814eac998ecff4323679eaff235ed
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-12-09 13:19:42 -0500

    Implement fetchMyIP in iss_promised & index2

--- iss_promised.js
+++ iss_promised.js
@@ -1,1 +1,11 @@
 const request = require('request-promise-native');
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+module.exports = { fetchMyIP };
