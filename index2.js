4124acb6543f30b863496bc3694607723789eb76
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-12-09 13:25:42 -0500

    Implemented fetchCoordsByIP into iss_promised & index2

--- index2.js
+++ index2.js
@@ -1,4 +1,8 @@
 const { fetchMyIP } = require('./iss_promised');
+const { fetchCoordsByIP } = require('./iss_promised');
+
 
 fetchMyIP()
+  .then(fetchCoordsByIP)
   .then(body => console.log(body));
+  
