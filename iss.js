d3fe08fd597cddead92b9b023a86cc4fe131b274
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-12-09 12:54:36 -0500

    Implemented fetchMyIP, & fetchCoordsByIP

---
+++ iss.js
@@ -0,0 +1,44 @@
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ * 
+ */
+const request = require('request');
+
+ const fetchMyIP = (callback) => {
+
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
+      return;
+    }
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://ipvigilante.com/${ip}`,  (error, resp, body) => {
+    if (error) return callback(error,null);
+
+    if (resp.statusCode !== 200) {
+      callback(Error(`Status Code ${resp.statusCode} when fetching coordinates for IP. Response: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body).data;
+
+    callback(null, { latitude, longitude });
+
+  });
+};
+
+module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
